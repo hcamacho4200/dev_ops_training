208d265e15a5833700c0511783897d31717fdb2e
Henry F. Camacho Jr <hcamacho@linkedin.com>
2021-10-11 19:27:53 -0500

    add support for updated documentation

--- src/api/v1/api_v1.py
+++ src/api/v1/api_v1.py
@@ -5,5 +5,5 @@ from .ep_hello import api as ns_ep_hello
 
 
 blueprint = Blueprint('Dev Ops Training', __name__, url_prefix='/api/v1')
-api = Api(blueprint, title='Dev Ops Training', version='V1.0', description='Dev Ops Training V1', )
+api = Api(blueprint, title='Dev Ops Training Test3', version='V1.0', description='Dev Ops Training V1', )
 api.add_namespace(ns_ep_hello)
